86ff0517cf32f09a7d1ef2c6c101ae35f6de6b02
symbat27 <242262@astanait.edu.kz>
2026-01-30 22:44:22 +0500

    Refactor main.go to set up HTTP server and handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"LotteryImitationSystem/internal/handlers"
+	"LotteryImitationSystem/internal/services"
+	"LotteryImitationSystem/internal/storage"
+)
 
 func main() {
-	fmt.Println("Lottery Imitation System started")
+	userRepo := storage.NewUserRepository()
+	ticketRepo := storage.NewTicketRepository()
+	prizeRepo := storage.NewPrizeRepository()
+
+	prizeChannel := make(chan string)
+
+	service := services.NewLotteryService(
+		userRepo,
+		ticketRepo,
+		prizeRepo,
+		prizeChannel,
+	)
+
+	go service.PrizeWorker()
+
+	userHandler := handlers.NewUserHandler(service)
+	ticketHandler := handlers.NewTicketHandler(service)
+	adminHandler := handlers.NewAdminHandler(service)
+
+	mux := http.NewServeMux()
+	userHandler.Register(mux)
+	ticketHandler.Register(mux)
+	adminHandler.Register(mux)
+
+	fs := http.FileServer(http.Dir("./internal/frontend"))
+	mux.Handle("/", fs)
+
+	log.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
