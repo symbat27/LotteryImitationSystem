e995bfa3643f4604819aae0b9204ec1581433f3c
symbat27 <242262@astanait.edu.kz>
2026-01-30 22:42:59 +0500

    Update prize.go

--- internal/models/prize.go
+++ internal/models/prize.go
@@ -1,7 +1,16 @@
 package models
 
+type PrizeType string
+
+const (
+	Money  PrizeType = "money"
+	Travel PrizeType = "travel"
+	Gift   PrizeType = "gift"
+)
+
 type Prize struct {
-	TicketID     int
-	MatchesCount int
-	Amount       float64
+	TicketID string    `json:"ticket_id"`
+	Type     PrizeType `json:"type"`
+	Name     string    `json:"name"`
+	Value    int       `json:"value"`
 }
