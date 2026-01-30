37d1d6df98012453e7b23dd2af73de7f5c8304fb
symbat27 <242262@astanait.edu.kz>
2026-01-30 22:43:39 +0500

    Implement LotteryService with prize distribution logic

--- internal/services/lottery_service.go
+++ internal/services/lottery_service.go
@@ -1,12 +1,116 @@
 package services
 
+import (
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"LotteryImitationSystem/internal/models"
+	"LotteryImitationSystem/internal/storage"
+	"LotteryImitationSystem/internal/utils"
+)
+
 type LotteryService struct {
+	users        *storage.UserRepository
+	tickets      *storage.TicketRepository
+	prizes       *storage.PrizeRepository
+	prizeChannel chan string
 }
 
-func (s *LotteryService) CreateTicket() {
-	// skeleton
+func NewLotteryService(
+	u *storage.UserRepository,
+	t *storage.TicketRepository,
+	p *storage.PrizeRepository,
+	ch chan string,
+) *LotteryService {
+	return &LotteryService{
+		users:        u,
+		tickets:      t,
+		prizes:       p,
+		prizeChannel: ch,
+	}
 }
 
-func (s *LotteryService) RunDraw() {
-	// skeleton
+func (s *LotteryService) PrizeWorker() {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for ticketID := range s.prizeChannel {
+		log.Println("Processing prize for ticket:", ticketID)
+
+		prizeTypes := []models.PrizeType{
+			models.Money,
+			models.Travel,
+			models.Gift,
+		}
+
+		prizeType := prizeTypes[rng.Intn(len(prizeTypes))]
+
+		var name string
+		var value int
+
+		switch prizeType {
+		case models.Money:
+			name = "Cash reward"
+			value = 100
+		case models.Travel:
+			name = "Trip to Paris"
+			value = 1000
+		case models.Gift:
+			name = "Smart Watch"
+			value = 300
+		}
+
+		prize := models.Prize{
+			TicketID: ticketID,
+			Type:     prizeType,
+			Name:     name,
+			Value:    value,
+		}
+
+		s.prizes.Save(prize)
+	}
+}
+
+func (s *LotteryService) CreateUser(username string) models.User {
+	user := models.User{
+		ID:        strconv.FormatInt(time.Now().UnixNano(), 10),
+		Username:  username,
+		CreatedAt: time.Now(),
+	}
+
+	s.users.Save(user)
+	return user
+}
+
+func (s *LotteryService) CreateTicket(userID string) models.Ticket {
+	ticket := models.Ticket{
+		ID:        strconv.FormatInt(time.Now().UnixNano(), 10),
+		UserID:    userID,
+		Numbers:   utils.RandomNumbers(),
+		CreatedAt: time.Now(),
+	}
+
+	s.tickets.Save(ticket)
+	s.prizeChannel <- ticket.ID
+
+	return ticket
+}
+
+func (s *LotteryService) Stats() map[string]int {
+	stats := map[string]int{
+		"money":  0,
+		"travel": 0,
+		"gift":   0,
+	}
+
+	for _, p := range s.prizes.List() {
+		stats[string(p.Type)]++
+	}
+
+	return stats
+}
+
+func (s *LotteryService) GetAllPrizes() []models.Prize {
+	return s.prizes.List()
 }
