6465f9b7f7b459bc6976b3c2b46f740bcd595ea0
zhannur17 <zannurmejrambek87@gmail.com>
2026-01-31 20:42:47 +0500

    Implement user registration handler

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -1,5 +1,36 @@
 package handlers
 
-func RegisterUser() {
-  // skeleton
+import (
+  "LotteryImitationSystem/internal/services"
+  "encoding/json"
+  "net/http"
+)
+
+type UserHandler struct {
+  s *services.LotteryService
+}
+
+func NewUserHandler(s *services.LotteryService) *UserHandler {
+  return &UserHandler{s}
+}
+
+func (h *UserHandler) Register(mux *http.ServeMux) {
+  mux.HandleFunc("/users", h.create)
+}
+
+func (h *UserHandler) create(w http.ResponseWriter, r *http.Request) {
+  var body struct {
+    Username string json:"username"
+  }
+
+  if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+    http.Error(w, "invalid JSON", http.StatusBadRequest)
+    return
+  }
+
+  user := h.s.CreateUser(body.Username)
+
+  if err := json.NewEncoder(w).Encode(user); err != nil {
+    http.Error(w, "failed to encode response", http.StatusInternalServerError)
+  }
 }
