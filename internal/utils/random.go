5099a042d68b193e1d3c4473e16bc3b401723203
zhannur17 <zannurmejrambek87@gmail.com>
2026-01-31 20:44:36 +0500

    Implement RandomNumbers function to generate random ints

--- internal/utils/random.go
+++ internal/utils/random.go
@@ -1,5 +1,16 @@
 package utils
 
-func GenerateRandomNumbers() []int {
-  return []int{}
+import (
+  "math/rand"
+  "time"
+)
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func RandomNumbers() []int {
+  return []int{
+    rng.Intn(50),
+    rng.Intn(50),
+    rng.Intn(50),
+  }
 }
