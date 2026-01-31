1db8281c5173cae1d43ed2b667c8eb033ee8cfed
GulnazNurseit <gulnaznurseit1227@gmail.com>
2026-01-31 12:44:19 +0500

    Refactor Ticket struct with new fields and types

--- internal/models/ticket.go
+++ internal/models/ticket.go
@@ -1,8 +1,12 @@
+ticket.go
+
 package models
 
+import "time"
+
 type Ticket struct {
-	ID              int
-	UserID          int
-	DrawID          int
-	SelectedNumbers []int
+  ID        string    json:"id"
+  UserID    string    json:"user_id"
+  Numbers   []int     json:"numbers"
+  CreatedAt time.Time json:"created_at"
 }
